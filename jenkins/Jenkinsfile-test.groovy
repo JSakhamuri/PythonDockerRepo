32a03a75a3b2de299fc83e9136e814de2e547c89
Jitendra Sakhamuri <jitendra@wiztechinc.com>
2022-01-05 17:38:33 -0500

    Update Jenkinsfile-test.groovy

--- jenkins/Jenkinsfile-test.groovy
+++ jenkins/Jenkinsfile-test.groovy
@@ -29,20 +29,6 @@ pipeline {
         }
       }
    
-         // Stopping Docker containers for cleaner Docker run
-     stage('stop previous containers') {
-         steps {
-            sh 'docker ps -f name=mypythonContainer -q | xargs --no-run-if-empty docker container stop'
-            sh 'docker container ls -a -fname=mypythonContainer -q | xargs -r docker container rm'
-         }
-       }
-      
-    stage('Docker Run') {
-     steps{
-         script {
-                sh 'docker run -d -p 8096:5000 --rm --name mypythonContainer 921066146026.dkr.ecr.us-east-2.amazonaws.com/docker-image-repo:latest'
-            }
-      }
-    }
+  
     }
 }
